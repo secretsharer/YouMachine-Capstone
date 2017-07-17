b7e0055f43eed3c2626e1e75c4beedaa21615125
Olivia <ssecretsharer@gmail.com>
2017-07-16 18:23:15 -0700

    commit first version of cosmic api message object

---
+++ config.js
@@ -0,0 +1,18 @@
+// config.js
+export default {
+  bucket: {
+    slug: process.env.COSMIC_BUCKET || 'cosmic-js-chat',
+    type_slug: 'messages'
+  }
+}
+
+config.js
+export default {
+  bucket: {
+    slug: process.env.COSMIC_BUCKET || 'ubot',
+    type_slug: 'messages'
+  },
+  server: {
+    host: process.env.https://youmachine.herokuapp.com/ || 'http://localhost:3000'
+  }
+}
